5d40fedd73389a9ad0e9524dcde40761b3988a18
G. Queiroz <gabrieljvnq@gmail.com>
2020-11-29 23:42:25 -0300

    Added src/prelude.rs

---
+++ datavir-fuse/src/prelude.rs
@@ -0,0 +1,7 @@
+pub use log::{debug, error, info, trace, warn};
+
+pub use rusqlite::params;
+pub use rusqlite::Connection;
+pub use rusqlite::Error as SQLError;
+pub use rusqlite::Result as SQLResult;
+pub use rusqlite::Transaction;
